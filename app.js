ba6fd9143e2702464d1a976d4165e3f22f99b794
kalyani-14-M <kalyanikalyani2445@gmail.com>
2025-02-13 09:13:59 +0530

    app.js

--- app.js
+++ app.js
@@ -7,16 +7,18 @@ const humidity = document.getElementById('humidity');
 const wind_speed = document.getElementById('wind-speed');
 const location_not_found = document.querySelector('.location-not-found');
 const weather_body = document.querySelector('.weather-body');
+const toggleBtn = document.getElementById('toggleBtn'); 
 
+let tempCelsius = null; 
+let isCelsius = true; 
 
-async function checkWeather(city){
+async function checkWeather(city) {
     const api_key = "910afcd2fdefb492b5e5cc49574b9a1e";
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}&units=metric`;
 
-    const weather_data = await fetch(`${url}`).then(response => response.json());
+    const weather_data = await fetch(url).then(response => response.json());
 
-
-    if(weather_data.cod === `404`){
+    if (weather_data.cod === `404`) {
         location_not_found.style.display = "flex";
         weather_body.style.display = "none";
         console.log("error");
@@ -26,14 +28,15 @@ async function checkWeather(city){
     console.log("run");
     location_not_found.style.display = "none";
     weather_body.style.display = "flex";
-    temperature.innerHTML = `${Math.round(weather_data.main.temp - 273.15)}°C`;
-    description.innerHTML = `${weather_data.weather[0].description}`;
 
+    tempCelsius = Math.round(weather_data.main.temp); 
+    updateTemperatureDisplay(); 
+
+    description.innerHTML = `${weather_data.weather[0].description}`;
     humidity.innerHTML = `${weather_data.main.humidity}%`;
     wind_speed.innerHTML = `${weather_data.wind.speed}Km/H`;
 
-
-    switch(weather_data.weather[0].main){
+    switch (weather_data.weather[0].main) {
         case 'Clouds':
             weather_img.src = "cloud.png";
             break;
@@ -49,12 +52,30 @@ async function checkWeather(city){
         case 'Snow':
             weather_img.src = "snow.png";
             break;
-
     }
 
     console.log(weather_data);
 }
 
-searchBtn.addEventListener('click', ()=>{
+
+function updateTemperatureDisplay() {
+    if (tempCelsius !== null) {
+        if (isCelsius) {
+            temperature.innerHTML = `${tempCelsius}°C`;
+        } else {
+            let tempFahrenheit = Math.round((tempCelsius * 9/5) + 32);
+            temperature.innerHTML = `${tempFahrenheit}°F`;
+        }
+    }
+}
+
+
+searchBtn.addEventListener('click', () => {
     checkWeather(inputBox.value);
+});
+
+
+toggleBtn.addEventListener('click', () => {
+    isCelsius = !isCelsius; 
+    updateTemperatureDisplay(); 
 });
